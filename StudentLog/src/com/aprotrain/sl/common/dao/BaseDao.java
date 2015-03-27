d70d627fb18deee1310ca593d4de332c347bb376
hoangdongtien <tien.hd90@gmail.com>
2015-03-27 18:54:53 +0700

    bootstrap add

---
+++ StudentLog/src/com/aprotrain/sl/common/dao/BaseDao.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.aprotrain.sl.common.dao;
+
+import java.util.List;
+
+/**
+ *
+ * @author admin
+ */
+public interface BaseDao<T> {
+    List<T> listAll();
+    T add();
+    T update(T a);
+    T delete(T a);
+    T saveOrUpdate(T a);
+}
