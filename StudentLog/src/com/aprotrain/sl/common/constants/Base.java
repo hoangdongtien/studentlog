88f6ad8b56b05f6b8d7fe7f8276069b19375196a
hoangdongtien <tien.hd90@gmail.com>
2015-04-06 08:58:05 +0700

    TienHd: update constants

--- StudentLog/src/com/aprotrain/sl/common/constants/Base.java
+++ StudentLog/src/com/aprotrain/sl/common/constants/Base.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.aprotrain.sl.common;
+package com.aprotrain.sl.common.constants;
 
 /**
  *
  * @author Tien Hoang D
  */
-public interface Constants {
+public interface Base {
     
 }
